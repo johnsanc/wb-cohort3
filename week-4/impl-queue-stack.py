38c5289c3fdd46bb984a6da3055306f378de6a3d
Johnny Sanchez <johnny.ss@icloud.com>
2019-07-22 23:53:25 -0700

    Added queue solution.

---
+++ week-4/impl-queue-stack.py
@@ -0,0 +1,49 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+        self.temp = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        for _ in range(len(self.queue) - 1): # leave last element
+            self.temp.append(self.queue.pop())
+        first = self.queue.pop()
+        
+        # put items back into queue
+        for x in self.temp:
+            self.queue.append(self.temp.pop())
+        
+        return first
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.queue) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
